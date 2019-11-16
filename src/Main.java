1b8dcbc69e49d0f418440e24c8fb82c32d6f64f5
nouredine amrane <amranenoureddin3@gmail.com>
2019-11-16 12:34:21 +0100

    Evo : read from file

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,32 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
-        int age = 2016; //Integer.parseInt(args[0]);
-        int limit = 100; // Integer.parseInt(args[1]);
+
+        Path currentRelativePath = Paths.get("");
+        String path = currentRelativePath.toAbsolutePath().toString();
+        try {
+            //List<String> allLines = Files.readAllLines(Paths.get(path + "/1.in"));
+            List<String> allLines = Files.readAllLines(Paths.get(path + "/2.in"));
+            for (String line : allLines) {
+                var arg = line.split(" ");
+                System.out.println(getMinBase(Integer.parseInt(arg[0]), Integer.parseInt(arg[1])));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static int getMinBase(int age, int limit) {
         int minAge = age, minBaseAge = 10;
         for (int i = 11; i < limit; i++) {
             var result = convertAgeFromBaseToBase(age, i);
@@ -15,7 +35,7 @@ public class Main {
                 minBaseAge = i;
             }
         }
-        System.out.println(minBaseAge);
+        return minBaseAge;
     }
 
     private static Integer convertAgeFromBaseToBase(int age, int base) {
